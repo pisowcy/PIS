138a45d77402f79537c3a6717a430174bbbd9ffd
rafauoo <rafal5094@gmail.com>
2023-10-31 23:23:56 +0100

    test

--- PIS/Website/tests.py
+++ PIS/Website/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from Website.models import Teacher
 
-# Create your tests here.
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Teacher.objects.create(name="Arnold Boczek", age=30)
+        Teacher.objects.create(name="Ferdynand Kiepski", age=35)
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Teacher.objects.get(name="Arnold Boczek")
+        cat = Teacher.objects.get(name="Ferdynand Kiepski")
+        self.assertEqual(lion.age, 30)
+        self.assertEqual(cat.age, 35)
