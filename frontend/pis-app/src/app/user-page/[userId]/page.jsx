ad654b0d779bf567187638b416d8418b3ee1d4b0
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-01-06 18:30:40 +0100

    Userpage data from backend added

--- frontend/pis-app/src/app/user-page/[userId]/page.jsx
+++ frontend/pis-app/src/app/user-page/[userId]/page.jsx
@@ -8,24 +8,33 @@ import { UserData } from "@/components/user-page/userdata";
 export default function Component({ params }) {
     const userId = params.userId
     const [userFavorites, setUserFavorites] = useState([])
+    const [user, setUser] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [loadingError, setLoadingError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            let favoriteFilms
-            try {
-            const responseFavorite = await fetch(`http://20.229.152.181/favoriteUserProductionByUser/${userId}`);
-            if (!responseFavorite.ok) {
-                throw new Error(`HTTP error! Status: ${responseFavorite.status}`);
-            }
-            const favoriteData = await responseFavorite.json();
+            let favoriteFilms;
+            let userData;
 
-            favoriteFilms = favoriteData.map((film) => {
-                return {
-                id: film.production
+            try {
+                const responseFavorite = await fetch(`http://20.229.152.181/favoriteUserProductionByUser/${userId}`);
+                if (!responseFavorite.ok) {
+                    throw new Error(`HTTP error! Status: ${responseFavorite.status}`);
                 }
-            })
+                const favoriteData = await responseFavorite.json();
+
+                const responseUser = await fetch(`http://20.229.152.181/users/${userId}`);
+                if (!responseUser.ok) {
+                    throw new Error(`HTTP error! Status: ${responseUser.status}`);
+                }
+                userData = await responseUser.json();
+
+                favoriteFilms = favoriteData.map((film) => {
+                    return {
+                    id: film.production
+                    }
+                })
 
             } catch (error) {
                 console.error("Error fetching data:", error);
@@ -34,7 +43,8 @@ export default function Component({ params }) {
                 setIsLoading(false);
             }
 
-            setUserFavorites(favoriteFilms)
+            setUserFavorites(favoriteFilms);
+            setUser(userData);
         };
     
         fetchData();
@@ -49,7 +59,7 @@ export default function Component({ params }) {
             alert('There is a problem with Your internet connection\nCheck it and try again')
             ) : (
                 <div className="flex flex-col gap-10 p-4 md:p-6">
-                <UserData />
+                <UserData email={user.email} username={user.username} />
                 <section className="grid gap-6">
                 <h2 className="text-2xl font-bold">Favorites</h2>
                 <div className="flex overflow-x-scroll gap-4 md:gap-6 lg:gap-8">
