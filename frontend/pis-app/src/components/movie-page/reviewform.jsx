3bd86906b8d6429cfd00923f24ea3bd9dbddab99
jkowalc5 <01169190@pw.edu.pl>
2024-01-06 18:58:29 +0100

    max review length

--- frontend/pis-app/src/components/movie-page/reviewform.jsx
+++ frontend/pis-app/src/components/movie-page/reviewform.jsx
@@ -4,65 +4,67 @@ import { Button } from "@/components/ui/button";
 import { Rating } from "@mui/material";
 
 export function ReviewForm({ movieId }) {
-    const [newReview, setNewReview] = useState("");
-    const [rating, setRating] = useState(0);
-  
-    const handleReviewChange = (event) => {
-      console.log("change:", event.target.value);
-      setNewReview(event.target.value);
-    };
-  
-    const handleReviewSubmit = async (event) => {
-      event.preventDefault();
-      console.log("New review:", newReview);
-  
-      try {
-        const response = await fetch("http://20.229.152.181/reviews", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            review: rating,
-            comment: newReview,
-            user: 1,
-            production: movieId,
-          }),
-        });
-  
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        alert("Your review has been successfully added!");
-        window.location.reload();
-      } catch (error) {
-        console.error("Error posting review:", error);
+  const [newReview, setNewReview] = useState("");
+  const [rating, setRating] = useState(0);
+
+  const handleReviewChange = (event) => {
+    setNewReview(event.target.value);
+  };
+
+  const handleReviewSubmit = async (event) => {
+    event.preventDefault();
+    console.log("New review:", newReview);
+
+    try {
+      const response = await fetch("http://20.229.152.181/reviews", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          review: rating,
+          comment: newReview,
+          user: 1,
+          production: movieId,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    };
-  
-    return (
-      <div className="mt-6 mx-auto w-2/3">
-        <h3 className="text-lg font-semibold mb-2">Write a review:</h3>
-        <Rating
-          name="simple-controlled"
-          value={rating}
-          size="large"
-          onChange={(event, newValue) => {
-            setRating(newValue);
-          }}
+      alert("Your review has been successfully added!");
+      window.location.reload();
+    } catch (error) {
+      console.error("Error posting review:", error);
+    }
+  };
+
+  return (
+    <div className="mt-6 mx-auto w-2/3">
+      <h3 className="text-lg font-semibold mb-2">Write a review:</h3>
+      <Rating
+        name="simple-controlled"
+        value={rating}
+        size="large"
+        onChange={(event, newValue) => {
+          setRating(newValue);
+        }}
+      />
+      <form className="flex flex-col space-y-4" onSubmit={handleReviewSubmit}>
+        <Textarea
+          placeholder="Type your review here."
+          onChange={handleReviewChange}
+          value={newReview}
+          maxLength={500}
         />
-        <form className="flex flex-col space-y-4" onSubmit={handleReviewSubmit}>
-          <Textarea
-            placeholder="Type your review here."
-            onChange={handleReviewChange}
-          />
-          <Button
-            className="w-1/2 py-2 text-white bg-blue-500 hover:bg-blue-700 rounded mx-auto"
-            type="submit"
-          >
-            Submit
-          </Button>
-        </form>
-      </div>
-    );
-  }
+        <div className="text-right text-sm">{newReview.length} / 500</div>
+        <Button
+          className="w-1/2 py-2 text-white bg-blue-500 hover:bg-blue-700 rounded mx-auto"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}
